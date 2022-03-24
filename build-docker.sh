96a9582e183d4bf8e197e7fbf6127c3e95a36894
flaport <floris.laporte@gmail.com>
2022-03-24 14:14:18 +0100

    build python 3.9

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,9 @@
 #/bin/sh
-if ! docker pull flaport/condalayout:$KLAYOUT_SEMVER > /dev/null 2> /dev/null; then
+if ! docker pull flaport/condalayout:$BUILD_SUFFIX > /dev/null 2> /dev/null; then
   export PYTHON_PYVER="$(echo "$PYTHON_SEMVER" | sed "s/\(^[0-9]\+\)\.\([0-9]\+\).*/\1\2/g")";
   export BUILD_SUFFIX="$KLAYOUT_SEMVER-$PYTHON_PYVER_$BUILD_NUMBER";
   export KLAYOUT_PYPI_LINK="$(cat klayout-pypi-links.txt | grep manylinux | grep $KLAYOUT_SEMVER | grep $PYTHON_PYVER | head -1)" && [ ! -z "$KLAYOUT_PYPI_LINK" ];
-  docker build . -t flaport/condalayout:$KLAYOUT_SEMVER \
+  docker build . -t flaport/condalayout:$BUILD_SUFFIX \
     --build-arg WORKERS=$WORKERS \
     --build-arg PYTHON_SEMVER=$PYTHON_SEMVER \
     --build-arg KLAYOUT_SEMVER=$KLAYOUT_SEMVER \
@@ -11,5 +11,5 @@ if ! docker pull flaport/condalayout:$KLAYOUT_SEMVER > /dev/null 2> /dev/null; t
     --build-arg PYTHON_PYVER=$PYTHON_PYVER \
     --build-arg BUILD_SUFFIX=$BUILD_SUFFIX \
     --build-arg KLAYOUT_PYPI_LINK=$KLAYOUT_PYPI_LINK;
-  docker push flaport/condalayout:$KLAYOUT_SEMVER
+  docker push flaport/condalayout:$BUILD_SUFFIX
 fi
